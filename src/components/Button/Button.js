1726921216cc0d853606358486f047fed50f56b8
maxcore25 <mkorobov2002@mail.ru>
2022-04-13 14:00:57 +0300

    Button Component Written

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import styles from './Button.module.css';
 
-export default function Button() {
-  return <button className={styles.button}>Button</button>;
+export default function Button({
+  onClick,
+  variant = '',
+  size = 'medium',
+  type = 'button',
+  children,
+}) {
+  return (
+    <button type={type} onClick={onClick} className={styles.button}>
+      {children}
+    </button>
+  );
 }
